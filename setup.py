9b502b057cd97e08aec6439db2f348ab5eb59361
Crassous Hadrien <crassoushadrien@gmail.com>
2025-11-26 14:40:47 +0100

    edit ai2thor version

--- setup.py
+++ setup.py
@@ -2,22 +2,15 @@
 
 from setuptools import setup, find_packages
 import os
-ABS_PATH = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(ABS_PATH, "AI2THOR_VERSION")) as f:
-    AI2THOR_VERSION = f.readlines()[0].strip()
 
-setup(name='thortils',
-      packages=find_packages(),
-      version='0.1',
-      description='Code related to Ai2-Thor. Try to do one thing once.',
-      python_requires='>3.6',
-      install_requires=[
-          'numpy',
-          'matplotlib',
-          'ai2thor=={}'.format(AI2THOR_VERSION),
-          'open3d',
-          'tqdm'
-      ],
-      license='MIT',
-      author='Kaiyu Zheng',
-      author_email='kaiyutony@gmail.com')
+setup(
+    name="thortils",
+    packages=find_packages(),
+    version="0.1",
+    description="Code related to Ai2-Thor. Try to do one thing once.",
+    python_requires=">3.6",
+    install_requires=["numpy", "matplotlib", "ai2thor==5.0.0", "open3d", "tqdm"],
+    license="MIT",
+    author="Kaiyu Zheng",
+    author_email="kaiyutony@gmail.com",
+)
