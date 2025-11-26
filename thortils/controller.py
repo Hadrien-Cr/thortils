bb439504dd1787cdd08e77f6ece121f0aafc8807
Hadrien Crassous <165465793+Hadrien-Cr@users.noreply.github.com>
2025-11-26 17:31:51 +0100

    Refactor import statements and launch_controller

--- thortils/controller.py
+++ thortils/controller.py
@@ -2,8 +2,8 @@
 # 
 # Usage of this file is licensed under the MIT License.
 import ai2thor.platform
-from ai2thor.controller import Controller
 from . import constants
+from ai2thor.controller import Controller
 
 def _resolve(event_or_controller):
     """Returns an event, whether the given parameter is an event (already)
@@ -45,24 +45,18 @@ def thor_controller_param(controller, param):
         return controller.height
     return controller.initialization_parameters[param]
 
+
 def launch_controller(config):
     controller = Controller(
         platform                   = ai2thor.platform.CloudRendering,
-        scene                      = config["scene"],
+        scene=config["scene"],
         agentMode                  = config.get("AGENT_MODE"                   ,constants.AGENT_MODE),
         gridSize                   = config.get("GRID_SIZE"                    ,constants.GRID_SIZE),
         visibilityDistance         = config.get("VISIBILITY_DISTANCE"          ,constants.VISIBILITY_DISTANCE),
         snapToGrid                 = config.get("SNAP_TO_GRID"                 ,constants.SNAP_TO_GRID),
         renderDepthImage           = config.get("RENDER_DEPTH"                 ,constants.RENDER_DEPTH),
         renderInstanceSegmentation = config.get("RENDER_INSTANCE_SEGMENTATION" ,constants.RENDER_INSTANCE_SEGMENTATION),
-        width                      = config.get("IMAGE_WIDTH"                  ,constants.IMAGE_WIDTH),
-        height                     = config.get("IMAGE_HEIGHT"                 ,constants.IMAGE_HEIGHT),
-        fieldOfView                = config.get("FOV"                          ,constants.FOV),
-        rotateStepDegrees          = config.get("H_ROTATION"                   ,constants.H_ROTATION),
-        x_display                  = config.get("x_display"                    , None),
-        host                       = config.get("host"                         , "127.0.0.1"),
-        port                       = config.get("port"                         , 0),
-        headless                   = config.get("headless"                     , False))
+    )
     return controller
 
 
